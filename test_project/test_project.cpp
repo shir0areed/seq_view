108659a8feda4317181bb210c13a0eae768ded83
Shiro A. Reed <38010555+shir0areed@users.noreply.github.com>
2024-06-23 20:46:15 +0900

    テストコード作成

--- test_project/test_project.cpp
+++ test_project/test_project.cpp
@@ -1,6 +1,78 @@
-﻿#include <iostream>
+﻿#include <seq_view.hpp>
+
+#include <iostream>
+
+using namespace SeqView;
+
+namespace
+{
+	void Print(const seq_view<int>& seq)
+	{
+		for (auto x : seq)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+
+	void PrintV(seq_view<int> seq)
+	{
+		for (auto x : seq)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+
+	void PrintPtr(const seq_view<const int*>& seq)
+	{
+		for (auto x : seq)
+			std::cout << *x << " ";
+		std::cout << std::endl;
+	}
+
+	void PrintR(const seq_view<int&>& seq)
+	{
+		for (auto& x : seq)
+			++x;
+		for (auto& x : seq)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+
+	void PrintStr(const seq_view<std::string>& seq)
+	{
+		for (auto x : seq)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+}
+
+#include <vector>
+#include <array>
+#include <unordered_map>
 
 int main()
 {
-
+	Print(seq_view<int>{std::vector<int>{5, 2, 3, 1, 4}});
+	PrintV(seq_view<int>{std::vector<int>{5, 2, 3, 1, 4}});
+	Print(seq_view<int>{std::array<int, 5>{4, 3, 5, 2, 1}});
+	Print(seq_view<int>{std::unordered_map<int, int>
+	{
+		{ 0, 3 },
+		{ 1, 1 },
+		{ 2, 2 },
+		{ 3, 4 },
+		{ 4, 5 },
+	}});
+	int ar[] = { 3, 4, 2, 5, 1 };
+	Print(ar);
+	seq_view<int> l1{ &ar[0], sizeof(ar) / sizeof(int) };
+	Print(l1);
+	seq_view<int>l2{ ar };
+	Print(l2);
+	const int* ar2[] = { &ar[0], &ar[3], &ar[2], &ar[1], &ar[4] };
+	PrintPtr(ar2);
+	seq_view<const int*>l3{ &ar2[0], sizeof(ar2) / sizeof(int*) };
+	PrintPtr(l3);
+	seq_view<const int*>l4{ ar2 };
+	PrintPtr(l4);
+	PrintR(ar);
+	PrintStr(std::vector<std::string>{ "foo", "bar", "buzz" });
 }
