cc1265bb8029636ab118bdf8578e2424208bf212
Shiro A. Reed <38010555+shir0areed@users.noreply.github.com>
2024-06-23 20:56:11 +0900

    size->numElems

--- seq_view/include/seq_view.hpp
+++ seq_view/include/seq_view.hpp
@@ -12,15 +12,15 @@ namespace SeqView
 		seq_view(const seq_view&) noexcept = default;
 
 		template<typename U>
-		seq_view(U&& elems, std::size_t size) noexcept
-			: pElems{ const_cast<typename std::decay<U>::type*>(&elems) }, pSubscript{ &Subscript<U> }, numElems{ size }
+		seq_view(U&& elems, std::size_t numElems) noexcept
+			: pElems{ const_cast<typename std::decay<U>::type*>(&elems) }, pSubscript{ &Subscript<U> }, numElems{ numElems }
 		{
 			static_assert(!std::is_pointer<typename std::decay<decltype(elems)>::type>::value, "pElems may not be pointer of pointer");
 		}
 
 		template<typename U>
-		seq_view(U* pElems, std::size_t size) noexcept
-			: pElems{ const_cast<typename std::remove_const<U>::type*>(pElems) }, pSubscript{ &SubscriptPtr<U> }, numElems{ size }
+		seq_view(U* pElems, std::size_t numElems) noexcept
+			: pElems{ const_cast<typename std::remove_const<U>::type*>(pElems) }, pSubscript{ &SubscriptPtr<U> }, numElems{ numElems }
 		{
 		}
 
