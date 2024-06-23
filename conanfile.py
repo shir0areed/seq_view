e1b0511a283a757a128a57e277327103858540d9
Shiro A. Reed <38010555+shir0areed@users.noreply.github.com>
2024-06-23 20:33:04 +0900

    テストプロジェクト追加

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,13 @@ from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
 
 class Recipe(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
+        
+    options = {
+        'build_test': [True, False],
+    }
+    default_options = {
+        'build_test': False,
+    }
 
     def requirements(self):
         pass
@@ -14,6 +21,7 @@ class Recipe(ConanFile):
         tc = CMakeToolchain(self)
         tc.user_presets_path = False
         tc.variables['CMAKE_POLICY_DEFAULT_CMP0091']='NEW'
+        tc.variables['SEQVIEW_BUILD_TEST'] = bool(self.options.build_test)
         tc.generate()
 
         deps = CMakeDeps(self)
